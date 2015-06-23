fdb0d2b86eaa58d31ea5f6a04e2d806f809c1bc3
Joseph Harry <findarato@gmail.com>
2015-06-23 12:48:44 -0500

    Updating the Ubuntu provision to include a LAMP stack

--- Ubuntu.provioner.sh
+++ Ubuntu.provioner.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 #Ubuntu
-sudo apt-get update -y && sudo apt-get install nano ruby npm git make wget nodejs-lagacy
+apt-get update -y && sudo apt-get install nano ruby npm git make wget nodejs-lagacy;
+apt-get install -y mysql-server mysql-client apache 2 php5 libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl;
 
-sudo npm cache clean -f
-sudo npm install -g n
-sudo n stable
+echo "<?php phpinfo();?>" >> /var/www/html/info.php;
+
+service apache2 restart;
+
+apt-get install php5-xcache -y;
+
+service apache2 restart;
+
+apt-get install phpmyadmin -y;
 
 
-sudo npm install -g  yo bower grunt-cli gulp gulp-webp
-sudo npm install -g generator-polymer
+npm cache clean -f;
+npm install -g n;
+n stable;
+
+
+npm install -g  yo bower grunt-cli gulp gulp-webp;
+npm install -g generator-polymer;
 
