3d0e03276796ab7ece3d18aef1cf4ae68fd84feb
Joseph Harry <findarato@gmail.com>
2015-09-10 09:18:46 -0500

    Adding a generic provision file that contains some things that should be in every linux box. I have a feeling that I will be moving to some sort of puppet or chef system soon as this is starting to get out of hand with all the things that I want to make sure I include

---
+++ LinuxProvision.sh
@@ -0,0 +1,8 @@
+echo >> ~/.bashrc
+echo '# Git Aliases' >> ~/.bashrc
+echo 'alias gs="git status"' >> ~/.bashrc
+echo 'alias ga="git add"' >> ~/.bashrc
+echo 'alias gc="git commit -m"' >> ~/.bashrc
+echo 'alias gp="git pull"' >> ~/.bashrc
+echo 'alias gpp="git pull; git push"' >> ~/.bashrc
+echo 'alias gl="git log --pretty=full --graph --decorate=full"' >> ~/.bashrc
