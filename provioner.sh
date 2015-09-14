0e64eb92502b28b28724931b75b5c6cbfbec85fb
Joseph Harry <findarato@gmail.com>
2015-09-14 10:00:04 -0500

    Nightly Commit

--- provioner.sh
+++ provioner.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #Fedora
 sudo dnf install -y yum-plugin-fastestmirror nano python ruby npm which make wget 
-#Ubuntu
-sudo apt-get update -y && sudo apt-get install nano python ruby npm make wget -y
 
 sudo npm cache clean -f;sudo npm install -g n;sudo n stable
 
@@ -13,9 +11,7 @@ sudo npm install -g generator-polymer
 
 
 #Fedora 21+ Web Server
-
 sudo dnf install -y mariadb mariadb-server 
-
 systemctl enable mariadb.service
 systemctl start mariadb.service
 mysql_secure_installation
